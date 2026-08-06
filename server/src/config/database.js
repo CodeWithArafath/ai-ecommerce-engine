49555dbaabc08dd8df7ac703d0b876e5d983a45d
Arafath <arafathmdarafath38550@gmail.com>
2026-08-06 21:14:21 +0530

    Added MongoDB models and database configuration

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
+﻿const mongoose=require("mongoose");
 
-const connectDB = async()=>{
+const connectDB=async()=>{
 
-try{
+ try{
 
-await mongoose.connect(process.env.MONGO_URI);
+ await mongoose.connect(process.env.MONGO_URI);
 
-console.log("MongoDB Connected");
+ console.log("MongoDB Connected");
 
-}catch(error){
+ }catch(error){
 
-console.log(error.message);
-process.exit(1);
+ console.log(error.message);
+ process.exit(1);
 
-}
+ }
 
 };
 
-module.exports = connectDB;
+module.exports=connectDB;
--- server/src/models/Product.js
+++ server/src/models/Product.js
@@ -1,55 +1,24 @@
-const mongoose = require("mongoose");
+﻿const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-    description: {
-      type: String,
-      required: true,
-    },
-
-    category: {
-      type: String,
-      required: true,
-      index: true,
-    },
-
-    brand: {
-      type: String,
-      default: "",
-    },
-
-    price: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-
-    stock: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-
-    images: [
-      {
-        type: String,
-      },
-    ],
-
-    embedding: {
-      type: [Number],
-      default: [],
-    },
-  },
-  {
-    timestamps: true,
-  }
+{
+ name:{
+  type:String,
+  required:true
+ },
+ description:String,
+ brand:String,
+ category:String,
+ price:Number,
+ stock:{
+  type:Number,
+  default:0
+ },
+ image:String
+},
+{
+ timestamps:true
+}
 );
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model("Product",productSchema);
